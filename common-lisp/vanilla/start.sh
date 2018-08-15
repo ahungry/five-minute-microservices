b72724f89ff8b7b47205a86a916fdebb791b9ced
Matthew Carter <m@ahungry.com>
2018-08-14 23:19:07 -0400

    Add Common Lisp sample

--- common-lisp/vanilla/start.sh
+++ common-lisp/vanilla/start.sh
@@ -2,8 +2,8 @@
 
 # -*- mode: shell -*-
 
-# TODO: Don't hardcode pwd
+set -x
 
-sbcl --eval '(push #P"~/src/five-minute-microservices/common-lisp/vanilla" ql:*local-project-directories*)' \
+sbcl --eval '(push #P"'$(pwd)'" ql:*local-project-directories*)' \
      --eval '(ql:quickload :5m-ms)' \
      --eval '(5m-ms:main)'
