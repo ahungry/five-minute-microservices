d0308d74d7f5efc6fb6b3aab5d32b1d146ac718f
Matthew Carter <m@ahungry.com>
2019-01-11 00:49:04 -0500

    Neat

--- elixir/vanilla/kv/README.md
+++ elixir/vanilla/kv/README.md
@@ -2,6 +2,8 @@
 
 **TODO: Add description**
 
+https://elixir-lang.org/getting-started/mix-otp/task-and-gen-tcp.html
+
 ## Installation
 
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed
@@ -18,4 +20,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/kv](https://hexdocs.pm/kv).
-
--- elixir/vanilla/kv/lib/kv.ex
+++ elixir/vanilla/kv/lib/kv.ex
@@ -1,4 +1,8 @@
 defmodule KV do
+  use Application
+
+  require KV.Server
+
   @moduledoc """
   Documentation for KV.
   """
@@ -14,5 +18,15 @@ defmodule KV do
   """
   def hello do
     :world
+  end
+
+  def start(_type, _args) do
+    children = [
+      {Task.Supervisor, name: KV.Server.TaskSupervisor},
+      {Task, fn -> KV.Server.accept(4040) end}
+    ]
+
+    opts = [strategy: :one_for_one, name: KV.Server.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
