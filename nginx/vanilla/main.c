c76ff457056e664ede0227500064402b1d5e6328
Matthew Carter <m@ahungry.com>
2019-01-26 02:10:48 -0500

    Simple but slow

--- nginx/vanilla/main.c
+++ nginx/vanilla/main.c
@@ -4,7 +4,12 @@
 int
 main (void)
 {
-  printf ("Content-type: application/json\r\nStatus: 200 OK\r\n\"0.0.1\"\r\n\r\n");
+  // printf ("Content-type: application/json\r\nStatus: 200 OK\r\n\"0.0.1\"\r\n\r\n");
+  printf ("HTTP/1.1 200 OK\n");
+  printf ("Content-Type: text/html\n");
+  printf ("Content-Length: 7\n");
+  printf ("Connection: close\n\n");
+  printf ("\"0.0.1\"");
 
   return 0;
 }
